5c0d719a0d64f2aa0d0075124d23e3d81c78e1d5
Alexandr <tav@digi-tech.dev>
2024-03-20 00:07:42 +0300

    Удаление ненужных файлов.

--- InventoryController/ItemController/Components/RDistatnt_Component.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class RDistatnt_Component : RItem_Component
-{
-    public Distant distant;
-}
--- InventoryController/ItemController/Components/Weapon_Component.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-public class Weapon_Component : RItem_Component
-{
-
-    public Weapon weapon;
-}
-
